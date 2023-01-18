87ad35fdc51ec2f389988fa879ba1b370dbb7e39
Jun0613-spec <kumo5110@gmail.com>
2023-01-18 10:10:12 +0000

    twitch ui clone

---
+++ data/MOCK_DATA.js
@@ -0,0 +1,99 @@
+export const rec_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/quiquiaamet.png?size=50x50&set=set1",
+    username: "tsuffield0",
+    game_name: "Transcof",
+    rank: 43,
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/etdolordelectus.png?size=50x50&set=set1",
+    username: "tbenadette1",
+    game_name: "Job",
+    rank: 10,
+  },
+  {
+    id: 3,
+    avatar:
+      "https://robohash.org/temporavoluptascommodi.png?size=50x50&set=set1",
+    username: "dperkin2",
+    game_name: "Vagram",
+    rank: 43,
+  },
+  {
+    id: 4,
+    avatar: "https://robohash.org/consequaturetaliquam.png?size=50x50&set=set1",
+    username: "dterrans3",
+    game_name: "Home Ing",
+    rank: 83,
+  },
+  {
+    id: 5,
+    avatar: "https://robohash.org/quismagnamsunt.png?size=50x50&set=set1",
+    username: "wgrumbridge4",
+    game_name: "Bamity",
+    rank: 27,
+  },
+  {
+    id: 6,
+    avatar: "https://robohash.org/cumpossimusvoluptas.png?size=50x50&set=set1",
+    username: "bdeblasi5",
+    game_name: "Bamity",
+    rank: 64,
+  },
+  {
+    id: 7,
+    avatar: "https://robohash.org/dolorpraesentiumesse.png?size=50x50&set=set1",
+    username: "cjouannin6",
+    game_name: "Stronghold",
+    rank: 28,
+  },
+  {
+    id: 8,
+    avatar: "https://robohash.org/autemetest.png?size=50x50&set=set1",
+    username: "taicken7",
+    game_name: "Fixflex",
+    rank: 62,
+  },
+  {
+    id: 9,
+    avatar: "https://robohash.org/eaquesuntvoluptas.png?size=50x50&set=set1",
+    username: "jmash8",
+    game_name: "Stim",
+    rank: 24,
+  },
+  {
+    id: 10,
+    avatar:
+      "https://robohash.org/officiareiciendisadipisci.png?size=50x50&set=set1",
+    username: "atoope9",
+    game_name: "Overhold",
+    rank: 8,
+  },
+];
+
+export const top_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/quiquiaamet.png?size=50x50&set=set1",
+    username: "tsuffield0",
+    game_name: "Transcof",
+    rank: 43,
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/etdolordelectus.png?size=50x50&set=set1",
+    username: "tbenadette1",
+    game_name: "Job",
+    rank: 10,
+  },
+  {
+    id: 3,
+    avatar:
+      "https://robohash.org/temporavoluptascommodi.png?size=50x50&set=set1",
+    username: "dperkin2",
+    game_name: "Vagram",
+    rank: 43,
+  },
+];
